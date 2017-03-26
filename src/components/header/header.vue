69e841d9169d826d12a224d6a32d4d7a7bdf4bd8
Joeng3ll <272679540@qq.com>
2017-03-26 20:53:27 +0800

    thirdDay

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -18,7 +18,7 @@
           <span class="supports-icon" :class="classMap[seller.supports[0].type]"></span>
           <p>{{seller.supports[0].description}}</p>
         </div>
-        <aside v-if="seller.supports" class="more-supports">
+        <aside v-if="seller.supports" class="more-supports" @click="showDetail()">
           <p class="count">{{seller.supports.length}}个</p>
           <i class="icon-keyboard_arrow_right"></i>
         </aside>
@@ -27,19 +27,44 @@
     <!--内容区结束-->
 
     <!--公告区开始-->
-    <aside class="bulletin-wrapper">
+    <aside class="bulletin-wrapper" @click="showDetail()">
       <span class="bulletin-icon"></span>
-      <p>{{seller.bulletin}}</p>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </aside>
     <!--公告区结束-->
+
+    <!--背景层开始-->
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <!--背景层结束-->
+
+    <!--弹出层开始-->
+    <div class="popop" v-show="detailShow">
+      <div class="detail-wrapper clearFix">
+        <div class="detail-main">
+          <section class="detail-header">
+            {{seller.name}}
+          </section>
+          <star-component :size="24" :score="seller.score"></star-component>
+        </div>
+      </div>
+
+      <div class="detail-close" @click="showDetail()">
+        <i class="icon-close"></i>
+      </div>
+    </div>
+    <!--弹出层结束-->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import StarComponent from 'components/star/star.vue'
   export default {
     data() {
       return {
+        detailShow: false,
         classMap: []
       }
     },
@@ -48,6 +73,14 @@
     ],
     created() {
       this.classMap = ['decrease-icon', 'discount-icon', 'special-icon', 'invoice-icon', 'guarantee-icon']
+    },
+    methods: {
+      showDetail: function () {
+        this.detailShow = !this.detailShow
+      }
+    },
+    components: {
+      'star-component': StarComponent
     }
   }
 
@@ -56,6 +89,7 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
   .header
+    position relative
     background: rgba(0, 0, 0, 0.5)
     font-size: 0
     color: rgb(255, 255, 255)
@@ -118,7 +152,7 @@
           line-height: 24px
           right: 12px
           bottom: 14px
-          padding: 5px 11px
+          padding: 3px 11px
           border-radius: 17px
           background: rgba(0, 0, 0, 0.2)
           p
@@ -132,20 +166,73 @@
             font-size 10px
 
     .bulletin-wrapper
-      height: 28px
-      padding: 4px 12px
-      overflow: hidden
-      background: rgba(0, 0, 0, 0.7)
-      p
-        display: inline
-        font-size: 10px
-        color: rgb(255, 255, 255)
-        font-weight: 100
-        line-height: 28px
+      display: flex
+      align-items center
+      position: relative
+      height 28px
+      line-height 28px
+      padding 0px 22px 0px 12px
+      background rgba(0, 0, 0, 0.5)
       .bulletin-icon
-        vertical-align top
+        display inline-block
+        width: 22px
+        height 12px
+        line-height 0
+        margin-right: 4px
+        bg-img('bulletin', 22px, 12px)
+      .bulletin-text
         display: inline-block
-        width: 33px
-        height: 18px
-        bg-img('bulletin', 33px, 18px)
+        width: 92%
+        font-size: 10px
+        font-weight 100
+        white-space nowrap
+        overflow hidden
+        text-overflow ellipsis
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 12px
+        bottom: 8px
+
+    .background
+      position absolute
+      display inline-block
+      width: 100%
+      height 100%
+      top: 0
+      left: 0
+      z-index: -1
+      filter: blur(10px)
+    .popop
+      position fixed
+      left 0
+      top 0
+      overflow auto
+      width 100%
+      height 100%
+      z-index 100
+      background rgba(7,17,27,0.8)
+      .detail-wrapper
+        width 100%
+        min-height 100%
+        .detail-main
+          margin-top 64px
+          padding 0 36px 64px 36px
+          font-size 12px
+          text-align center
+          .detail-header
+            width 100%
+            font-size 16px
+            font-weight 350
+            text-align center
+            margin-bottom 16px
+      .detail-close
+        width 32px
+        height 32px
+        position relative
+        margin-top -64px
+        left 50%
+        .icon-close
+          font-size 32px
+          color: rgba(255,255,255,0.5)
+
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="starClass in classes" :class="starClass" class="star-item">
+    </span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const ON = 'on'
+  const HALF = 'half'
+  const OFF = 'off'
+  const starNumber = 5
+  export default {
+    props: ['size', 'score'],
+    computed: {
+      starType: function () {
+        return 'starType-' + this.size
+      },
+      classes: function () {
+        let starArr = []
+        for (var i = 0; i < Math.floor(this.score); i++) {
+          starArr.push(ON)
+        }
+        if (Math.floor(this.score) % 2 !== 0) {
+          starArr.push(HALF)
+        }
+        if (starArr.length < starNumber) {
+          starArr.push(OFF)
+        }
+        return starArr
+      }
+    }
+
+  }
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/base.styl"
+  @import "../../common/stylus/mixin.styl"
+  .star
+    &.starType-24
+      & > .star-item
+        display inline-block
+        starStyle(10px, 9.5px, 3.25px)
+        &.on
+          bg-img('star24_on', 10px, 9.5px)
+        &.off
+          bg-img('star24_off', 10px, 9.5px)
+        &.half
+          bg-img('star24_half', 10px, 9.5px)
+    &.starType-36
+      & > .star-item
+        display inline-block
+        starStyle(15px, 14.3px, 6.25px)
+        &.on
+          bg-img('star36_on', 15px, 14.3px)
+        &.off
+          bg-img('star36_off', 15px, 14.3px)
+        &.half
+          bg-img('star36_half', 15px, 14.3px)
+    &.starType-48
+      & > .star-item
+        display inline-block
+        starStyle(30px, 28.5px, 9.375px)
+        &.on
+          bg-img('star48_on', 30px, 28.5px)
+        &.off
+          bg-img('star48_off', 30px, 28.5px)
+        &.half
+          bg-img('star48_half', 30px, 28.5px)
+</style>
