0d7968051c8d2f88bcb88781a1ea25dc64bdc2bc
Joeng3ll <272679540@qq.com>
2017-04-24 14:26:17 +0800

    Seller

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    it's seller component
+  <div class="seller-wrapper" ref="sellerWrapper">
+    <div class="scroll-box">
+      dd
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-      name: 'app'
+  import BScroll from 'better-scroll'
+  export default {
+    data() {
+      return {
+        seller: Object
+      }
+    },
+    created() {
+      this.$http.get('/api/seller').then((res) => {
+//        console.log(res)
+        if (res.data.errno === 0) {
+          res = res.data
+          this.seller = res.data
+        }
+      })
+    },
+    methods: {
+      _initialScroll() {
+        if (!this.sellerScroll && this.$refs.sellerWrapper) {
+          this.sellerScroll = new BScroll(this.$refs.sellerWrapper, {click: true})
+        } else {
+          this.sellerScroll.refresh()
+        }
+      }
     }
+  }
 
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .seller-wrapper
+    width 100%
+    position absolute
+    top 174px
+    bottom 52px
 </style>
